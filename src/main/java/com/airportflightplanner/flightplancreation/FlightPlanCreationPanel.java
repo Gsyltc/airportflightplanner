306f220585048f15bc1ea066dee03204102625a1
sylvaPortable <sylvain.goubaud@hotmail.fr>
2016-07-01 20:42:37 +0200

    Dialog

--- src/main/java/com/airportflightplanner/flightplancreation/FlightPlanCreationPanel.java
+++ src/main/java/com/airportflightplanner/flightplancreation/FlightPlanCreationPanel.java
@@ -4,6 +4,11 @@
  */
 package com.airportflightplanner.flightplancreation;
 
+import java.awt.Component;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 
@@ -21,91 +26,119 @@ import com.jgoodies.forms.layout.RowSpec;
  */
 public class FlightPlanCreationPanel extends FormDebugPanel {
 
-    /**
-     *
-     */
-    private static final long              serialVersionUID = 4047549681152943474L;
-    /**
-     *
-     */
-    private final FlighPlanCollectionModel flightPlansCollection;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 4047549681152943474L;
+	/**
+	 *
+	 */
+	private final FlighPlanCollectionModel flightPlansCollection;
+	/** */
+	private JComboBox<String> routeSelector;
 
-    /**
-    *
-    */
+	/**
+	*
+	*/
 
-    /**
-     * @param flightPlansCollection
-     *
-     */
-    public FlightPlanCreationPanel(final FlighPlanCollectionModel flightPlansCollection) {
-        this.flightPlansCollection = flightPlansCollection;
-        buildPanel();
-    }
+	/**
+	 * @param flightPlansCollection
+	 *
+	 */
+	public FlightPlanCreationPanel(final FlighPlanCollectionModel flightPlansCollection) {
+		this.flightPlansCollection = flightPlansCollection;
+		buildPanel();
+	}
 
-    /**
-    *
-    */
-    private void buildPanel() {
-        setLayout(new FormLayout(new ColumnSpec[] { //
-                FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
-                FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
-                FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
-                FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
-                FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
-                FormSpecs.RELATED_GAP_COLSPEC, //
-                ColumnSpec.decode("pref:grow"), //
-                FormSpecs.RELATED_GAP_COLSPEC, }, //
-                new RowSpec[] { FormSpecs.RELATED_GAP_ROWSPEC, //
-                        FormSpecs.PREF_ROWSPEC, //
-                        FormSpecs.RELATED_GAP_ROWSPEC, //
-                        FormSpecs.PREF_ROWSPEC, //
-                        FormSpecs.RELATED_GAP_ROWSPEC, //
-                        FormSpecs.PREF_ROWSPEC, //
-                        FormSpecs.RELATED_GAP_ROWSPEC, }));
+	/**
+	*
+	*/
+	private void buildPanel() {
+		setLayout(new FormLayout(
+				new ColumnSpec[] { //
+						FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
+						FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
+						FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
+						FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
+						FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("pref:grow"), //
+						FormSpecs.RELATED_GAP_COLSPEC, //
+						ColumnSpec.decode("pref:grow"), //
+						FormSpecs.RELATED_GAP_COLSPEC, }, //
+				new RowSpec[] { FormSpecs.RELATED_GAP_ROWSPEC, //
+						FormSpecs.PREF_ROWSPEC, //
+						FormSpecs.RELATED_GAP_ROWSPEC, //
+						FormSpecs.PREF_ROWSPEC, //
+						FormSpecs.RELATED_GAP_ROWSPEC, //
+						FormSpecs.PREF_ROWSPEC, //
+						FormSpecs.RELATED_GAP_ROWSPEC, //
+						FormSpecs.PREF_ROWSPEC, //
+						FormSpecs.RELATED_GAP_ROWSPEC, }));
 
-        JLabel startLabel = new JLabel(FlightPlanCreationPanelMessages.START_LABEL);
-        add(startLabel, "2,2,3,1");
-        add(createStartTextField(), "2,4,3,1");
+		JLabel startLabel = new JLabel(FlightPlanCreationPanelMessages.START_LABEL);
+		add(startLabel, "2,2,3,1");
+		add(createStartTextField(), "2,4,3,1");
 
-        JLabel endLabel = new JLabel(FlightPlanCreationPanelMessages.END_LABEL);
-        add(endLabel, "6,2,4,1");
-        add(createEndTextField(), "6,4,3,1");
+		JLabel endLabel = new JLabel(FlightPlanCreationPanelMessages.END_LABEL);
+		add(endLabel, "6,2,3,1");
+		add(createEndTextField(), "6,4,3,1");
 
-        JLabel timeLabel = new JLabel(FlightPlanCreationPanelMessages.TIME_LABEL);
-        add(timeLabel, "10,2,4,1");
-        add(createTimeTextField(), "10,4,3,1");
+		JLabel timeLabel = new JLabel(FlightPlanCreationPanelMessages.TIME_LABEL);
+		add(timeLabel, "10,2,3,1");
+		add(createTimeTextField(), "10,4,3,1");
 
-    }
+		JLabel routeLabel = new JLabel(FlightPlanCreationPanelMessages.ROUTE_LABEL);
+		add(timeLabel, "2,6,5,1");
+		add(createRouteSelectorCombo(), "2,8,5,1");
 
-    /**
-     *
-     * @return
-     */
-    private JFormattedTextField createTimeTextField() {
-        JFormattedTextField textField = new JFormattedTextField();
+	}
 
-        return textField;
-    }
+	/**
+	 * 
+	 * @return
+	 */
+	private JComboBox<String> createRouteSelectorCombo() {
+		routeSelector = new JComboBox<>();
+		routeSelector.addItemListener(new ItemListener() {
 
-    /**
-     *
-     * @return
-     */
-    private JFormattedTextField createEndTextField() {
-        JFormattedTextField textField = new JFormattedTextField();
+			/** */
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				// TODO Auto-generated method stub
 
-        return textField;
-    }
+			}
+		});
 
-    /**
-     *
-     * @return
-     */
-    private JFormattedTextField createStartTextField() {
-        JFormattedTextField textField = new JFormattedTextField();
+		return routeSelector;
+	}
 
-        return textField;
-    }
+	/**
+	 *
+	 * @return
+	 */
+	private JFormattedTextField createTimeTextField() {
+		JFormattedTextField textField = new JFormattedTextField();
+
+		return textField;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	private JFormattedTextField createEndTextField() {
+		JFormattedTextField textField = new JFormattedTextField();
+
+		return textField;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	private JFormattedTextField createStartTextField() {
+		JFormattedTextField textField = new JFormattedTextField();
+
+		return textField;
+	}
 
 }
