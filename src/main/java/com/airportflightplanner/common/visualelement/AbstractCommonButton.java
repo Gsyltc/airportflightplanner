e65859e6bf96357127f680b7ce02a5d6b4dcadd7
sylvaPortable <sylvain.goubaud@hotmail.fr>
2016-07-22 07:32:47 +0200

    Ajout du comportement des button pour le panneau des options

---
+++ src/main/java/com/airportflightplanner/common/visualelement/AbstractCommonButton.java
@@ -0,0 +1,30 @@
+/// *
+// * @(#)AbstractCommonButton.java
+// *
+// * Goubaud Sylvain - 2016.
+// *
+// * This code may be freely used and modified on any personal or professional
+// * project. It comes with no warranty.
+// *
+// */
+//
+// package com.airportflightplanner.common.visualelement;
+//
+// import javax.swing.JButton;
+//
+// import org.apache.commons.logging.Log;
+// import org.apache.commons.logging.LogFactory;
+//
+/// **
+// * @author Goubaud Sylvain
+// *
+// */
+// public class CommonButton extends JButton {
+//
+// /**
+// *
+// */
+// public CommonButton() {
+// super(arg0) }
+//
+// }
