947301fd096ca0dbf8c4dac31e2b8a36fee08a66
sylvaPortable <sylvain.goubaud@hotmail.fr>
2016-07-22 07:32:47 +0200

    Ajout du comportement des button pour le panneau des options

---
+++ src/main/java/com/airportflightplanner/common/types/ActionTypes.java
@@ -0,0 +1,21 @@
+/* @(#)ActionTypes.java
+ *
+ * Copyright (c) 2016 DCNS SA. All rights reserved.
+ * DCNS PROPRIETARY/CONFIDENTIAL.  Use is subject to license terms.
+ *
+ * This file, together  with  its accompanying  software product  and
+ * documentation, is  protected by the  intellectual  property rights
+ * in  France  and  other  countries, any  applicable  copyrights  or
+ * patent rights, and international treaty provisions. No part may be
+ * reproduced  in  any  form  by  any  mean  without   prior  written
+ * authorization of DCNS.
+ */
+package com.airportflightplanner.common.types;
+
+/**
+ * @author DCNS
+ *
+ */
+public enum ActionTypes {
+    VALIDATE, CANCEL, REFRESH
+}
